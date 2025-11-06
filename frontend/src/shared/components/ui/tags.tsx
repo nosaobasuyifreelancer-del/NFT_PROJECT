e7a1753a1d3b29f6ffde99e93efb971e78c001db
adeke910 <adekemibamiteko@gmail.com>
2025-11-06 18:14:12 +0100

    removed

--- frontend/src/shared/components/ui/tags.tsx
+++
@@ -1,7 +0,0 @@
-export default function Tag({ text }: { text: string }) {
-  return (
-    <div className="flex items-center w-fit whitespace-nowrap rounded-xs px-1.5 py-1 h-[18px] justify-center border border-border-1 bg-bg-primary font-mono uppercase text-xs">
-      {text}
-    </div>
-  );
-}
